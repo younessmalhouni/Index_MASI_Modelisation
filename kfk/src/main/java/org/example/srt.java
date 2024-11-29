4411d0f75b964d12d96f67b538e445c69437a3b2
Taha Ferhan <132760925+TFerhan@users.noreply.github.com>
2024-11-29 18:00:49 +0100

    Update srt.java

--- kfk/src/main/java/org/example/srt.java
+++ kfk/src/main/java/org/example/srt.java
@@ -1,37 +1,66 @@
 package org.example;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.kafka.clients.producer.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 import org.json.*;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.Properties;
 
 public class srt {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Properties prop = new Properties();
 
         prop.put("bootstrap.servers", "localhost:9092");
-        prop.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
-        prop.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
-        prop.put("schema.registry.url", "http://localhost:8081");
+        prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+
 
         String topic = "casa-bourse";
 
-        Producer<String, BourseData> producer = new KafkaProducer<>(prop);
+        Producer<String, byte[]> producer = new KafkaProducer<>(prop);
+
+
 
 
 
         JSONArray data = getData.aralya_data();
         JSONObject first_dt = (JSONObject) data.get(0);
 
+
         BourseData bourseData = new BourseData();
         for(String key : first_dt.keySet()){
             bourseData.put(key, first_dt.get(key));
         }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DatumWriter<BourseData> datumWriter = new SpecificDatumWriter<>(BourseData.class);
+        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        datumWriter.write(bourseData, encoder);
+        encoder.flush();
+        byte[] avroData = out.toByteArray();
+
+
+        ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, "ADH",  avroData);
+        producer.send(record);
+
+        producer.close();
+
+        System.out.println("Message sent to topic: " + topic);
+
 
 
 
